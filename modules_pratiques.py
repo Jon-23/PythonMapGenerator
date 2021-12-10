6b03c2491686ee94f0d68b278641170116cb2f5e
Jonathan NEVEU <JNeveu@dynamit.fr>
2021-12-10 16:25:43 +0100

    Correction du module Calendrier

--- modules_pratiques.py
+++ modules_pratiques.py
@@ -7,30 +7,44 @@ def est_dans_liste(element,liste):
     return {'reponse':False,'position':-1}
 
 class Calendrier:
-
-    def _retire_le_reste_du_divisible(self,somme,divisible,rendu_simple=True):
-        if somme < 0:
-            somme%0
-        reste_du_divisible = somme%divisible
-        somme -= reste_du_divisible
-        if rendu_simple:
-            return {'restant_somme':somme,'reste_du_divisible':reste_du_divisible}
-        else:
-            return somme,reste_du_divisible
         
     def secondes_en_date(self,secondes_p):
         valeur_en_secondes = {'minute':60,
                               'heure':(60*60),
                               'jour':((60*60)*24)}
+        annee = 1970
         secondes_p = int(secondes_p)
-        secondes_p,secondes = self._retire_le_reste_du_divisible(secondes_p,valeur_en_secondes['minute'],False)
-        secondes_p,minutes = self._retire_le_reste_du_divisible(secondes_p,valeur_en_secondes['heure'],False)
-        secondes_p,heure = self._retire_le_reste_du_divisible(secon<des_p,valeur_en_secondes['jour'],False)
+        secondes = secondes_p%60
+        secondes_p = secondes_p-secondes
+        minutes = (secondes_p%(60*60))//60
+        secondes_p = secondes_p-(secondes_p%(60*60))
+
+        heure = (secondes_p%((60*60*24)))//(60*60)
+        secondes_p = secondes_p-(secondes_p%(60*60*24))
+        jours = secondes_p//(60*60*24)
+
         
-        return {'heures':heure,'minutes':minutes,'secondes':secondes}
-    
-    def maintenant(self):
-        print("TODO")
+        annee_minimum = secondes_p//31536000
+        jours_par_annee = []
+        
+        return {'jours':jours,'heures':heure,'minutes':minutes,'secondes':secondes}
+
+    def _zero_devant(self,nombre):
+        nombre = int(nombre)
+        if nombre < 10:
+            return "0"+str(nombre)
+        return nombre
+    def maintenant(self,format_zero = False):
+        date = time.localtime()
+        retour_date = {}
+        retour_date['annee'] = date[0]
+        retour_date['mois'] = date[1]
+        retour_date['jour'] = date[2]
+        retour_date['heure'] = self._zero_devant(date[3])
+        retour_date['minute'] = self._zero_devant(date[4])
+        retour_date['seconde'] = self._zero_devant(date[5])
+        return retour_date
+
             
     def est_bixestille(self,annee):
         if (annee%4)==0:
